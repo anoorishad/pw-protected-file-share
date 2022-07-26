eab1c3df05118205839fd7a39e2aec4105258b3f
anoorishad <anoorishad@me.com>
2022-07-26 15:02:29 -0700

    Added password hashing using bcrypt

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+require("dotenv").config()
 const multer = require("multer")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const File = require("./models/File")
 
 const express = require("express")
 const app = express()
 
 const upload = multer({ dest: "uploads" })
+
+mongoose.connect(process.env.DATABASE_URL)
 
 app.set("view engine", "ejs")
 
@@ -11,8 +17,18 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.post("/upload", upload.single("file"), (req, res) => {
-    res.send("hi")
+app.post("/upload", upload.single("file"), async (req, res) => {
+    const fileData = {
+        path: req.file.path,
+        originalName: req.file.originalname
+    }
+    if (req.body.password != null && req.body.password !== "") {
+        fileData.password = await bcrypt.hash(req.body.password, 10)
+    }
+
+    const file = await File.create(fileData)
+    console.log(file)
+    res.send(file.originalName)
 })
 
-app.listen(3000)
+app.listen(process.env.PORT)
